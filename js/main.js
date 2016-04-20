d9d92ded0df7f5561fb4af4e0b38905205881a6c
AlexandraRothenberg <alexandra.rothenberg12@gmail.com>
2016-04-20 14:32:16 -0400

    banner and beginning of tips done

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var $banner = $('.banner');
+var $width = $banner.outerWidth();
+var $height = $banner.outerHeight();
+var $makeSnow;
+
+for (i = 0; i < 30; i++) {
+
+  $makeSnow = $('<div>');
+  $makeSnow.addClass('snow');
+  $makeSnow.css({
+    top: Math.random() * $height,
+    left: Math.random() * $width,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')',
+    'animation-duration': Math.floor(Math.random() * (5.5 - 2.5 + 1)) + 2.5 + 's'
+
+  });
+
+  $banner.append($makeSnow);
+
+};
